b6be526e3d2ffb29f0320992633573962d3d49a8
Sahil Latif <sahillatif072@gmail.com>
2026-08-16 14:59:07 +0500

    teacher name issue fixed

--- src/component/SingleClass.js
+++ src/component/SingleClass.js
@@ -4,9 +4,9 @@ const SingleClass = ({cl}) => {
     
   return (
     <div className="single-class">
-        <span id="class-name">{cl.val.split('\n')[0]}</span>
+        <span id="class-name">{cl.val.split('\n')[0] + " " + cl.val.split('\n')[1]}</span>
         <span className="class-location text">Location: {cl.location}</span>
-        <span className="class-teacher text">Instructor: {cl.val.split('\n')[1]}</span>
+        <span className="class-teacher text">Instructor: {cl.val.split('\n')[2]}</span>
         <span className="class-time text">Starting Slot: {cl.location==="City Campus"?cl.val.split('\n')[2]:cl.slot}</span>
     </div>
   )
